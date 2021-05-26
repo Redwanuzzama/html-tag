6885e43920492a36f514f89a2a6eb1972e07970d
Redwanuzzama <84873442+Redwanuzzama@users.noreply.github.com>
2021-05-27 01:24:22 +0600

    just only

---
+++ BioData.java
@@ -0,0 +1,11 @@
+package basicjava;
+
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Redwnuzzaman \n01625830732 \nBSC in CSE");
+        System.out.println("01625830732");
+        System.out.println("BSC in CSE");
+        System.out.println("\"Java program\"");
+    }
+    
+}
